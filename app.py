f118b376d6f4e3e259fe68a372e67ea8363b9ced
pocom9597-debug <pocom9597@gmail.com>
2025-09-07 23:55:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-import asyncio
-import time
+import random
+import requests
+import json
 
-async def main():
-    while True:
-        print("✅ Worker is running...")
-        await asyncio.sleep(5)
+url = "https://www.ttboost.app/api/coins/check"
 
-if __name__ == "__main__":
-    asyncio.run(main())
+
+headers = {
+  'User-Agent': "ktor-client",
+  'Accept': "application/json",
+  'Accept-Encoding': "gzip",
+  'Content-Type': "application/json",
+  'authorization': "Bearer 637808|TDGqqu8tmBafB9B6ayFZv7RDdXZDtb9MpDQHdQHO6d135f9e",
+  'accept-charset': "UTF-8"
+}
+while True:
+    payload = {
+    "order_id": random.randint(1000000, 1899999),
+    "username": "jdhddjdjhdn",
+    "likes_incremented": None,
+    "profile_username": "777mrmydohe",
+    "sec_uid":"MS4wLjABAAAA4o1aH5nuax0I6UMEhAnb2bxT8FOwqaooWdieB1sSV1ZA0b5w7Yd_FIDn0rruKzv7"
+    }
+    res = requests.post(url, data=json.dumps(payload), headers=headers).json()
+    if res["success"]==True:
+        print("done add coins : "+str(res["data"]["coins_earned"]))
+    else:
+        print("error add")
